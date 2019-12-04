78205da2fb2083e60a0cac07193657aaaa446d74
Darren Redmond <darren.redmond@gmail.com>
2019-12-04 20:51:21 +0000

    add menu functions and tests for write_file and get_message.

--- project/bill_management.py
+++ project/bill_management.py
@@ -4,13 +4,40 @@ Created on Wed Dec  4 18:34:36 2019
 
 @author: 99993
 """
-
 def read_bills():
-    bills = []
-    bills_file = open('bills.csv')
-    for line in bills_file:
-        if len(line) > 1:
-            bills.append(line.strip().split(','))
-            bills[-1][-1] = bills[-1][-1].strip()
-    return bills
+    return [[col.strip() for col in row.strip().split(',')]
+             for row in open('bills.csv') if len(row) > 1]
 
+def write_bills(bills):
+    bill_file = open('bills.csv', 'w')
+    for bill in bills:
+        bill_file.write(', '.join(bill) + '\n')
+
+def get_message():
+    return 'Hello, Welcome to the Bill Management Company\n' + \
+        '1: View Bills\n2: Insert a Bill\n3: Reports\n4: T&Cs\n5: Exit'
+
+def display_menu():
+    print(get_message())
+
+def view_bills(bills):
+    for bill in bills:
+        print(bill[0], bill[1], bill[2], bill[3], bill[4], bill[5], bill[6])
+    
+def process_choice(bills):
+    choice = input('Please enter an option:')
+    while choice != '5':
+        if choice == '1':
+            view_bills(bills)
+        elif choice == '4':
+            print('The terms of the billing management company')
+        choice = input('Please enter an option:')
+
+def main():
+    bills = read_bills()
+    display_menu()
+    process_choice(bills)
+    write_bills(bills)
+    
+if __name__ == '__main__':
+    main()
--- project/bill_management_test.py
+++ project/bill_management_test.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 
+# https://github.com/darrenredmond/B9DA100-DR/tree/master/project
+
 import unittest
 
-from bill_management import read_bills
+from bill_management import get_message, read_bills, write_bills
 
 class TestBillManagement(unittest.TestCase):
 
-    def test_bill_management(self):
+    def test_read_bills(self):
         bills = read_bills()
-        self.assertEquals(20, len(bills))
-        self.assertEquals('Electric Ireland', bills[0][0])
-        self.assertEquals('credit', bills[19][6])
+        self.assertEqual(20, len(bills))
+        self.assertEqual('Electric Ireland', bills[0][0])
+        self.assertEqual('credit', bills[19][6])
+
+    def test_write_bills(self):
+        bills = read_bills()
+        write_bills(bills)
+        bills = read_bills()
+        self.assertEqual(20, len(bills))
+        self.assertEqual('Electric Ireland', bills[0][0])
+        self.assertEqual('credit', bills[19][6])
+
+    def test_get_message(self):
+        self.assertEqual('Hello, Welcome to the Bill Management Company\n' + \
+                         '1: View Bills\n2: Insert a Bill\n3: Reports\n4: T&Cs\n5: Exit',
+            get_message())
 
 if __name__ == '__main__':
     unittest.main()
